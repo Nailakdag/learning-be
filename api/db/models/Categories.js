454393fee9d7bbd5b9422aed67d3c54df157b62b
Nail <nail.akdag@invivosoftware.com>
2024-08-06 13:26:15 +0300

    apis get post delete update created.

--- api/db/models/Categories.js
+++ api/db/models/Categories.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const schema = mongoose.Schema({
+    name: { type: String, required: true },
     is_active: { type: Boolean, default: true },
     created_by: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
+        type: mongoose.Schema.Types.ObjectId
     },
 
 }, {
